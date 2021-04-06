43d9fbdda1d1ba6df7b7e56caa8b1fb2a77191b4
Ladizzz <v.bobrovko@yandex.by>
2021-04-06 09:16:09 +0300

    Flask support added

--- parser.py
+++ parser.py
@@ -200,7 +200,7 @@ def main():
 
         server = Flask(__name__)
 
-        @server.route('/' + token, methods=['POST'])
+        @server.route('/bot', methods=['POST'])
         def get_message():
             json_string = request.get_data().decode('utf-8')
             update = telebot.types.Update.de_json(json_string)
@@ -211,7 +211,7 @@ def main():
         def webhook():
             bot.remove_webhook()
             bot.set_webhook(
-                url="https://boiling-ridge-34241.herokuapp.com/")  # этот url нужно заменить на url вашего Heroku приложения
+                url="https://boiling-ridge-34241.herokuapp.com/bot")  # этот url нужно заменить на url вашего Heroku приложения
             return "?", 200
 
         server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
