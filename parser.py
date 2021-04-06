ff652e40e97a60b990ca2f8aaf596a66ffc70683
Ladizzz <v.bobrovko@yandex.by>
2021-04-06 14:45:13 +0300

    .

--- parser.py
+++ parser.py
@@ -212,7 +212,7 @@ def main():
         def webhook():
             bot.remove_webhook()
             bot.set_webhook(
-                url="https://boiling-ridge-34241.herokuapp.com/bot")  # этот url нужно заменить на url вашего Хероку приложения
+                url="https://boiling-ridge-34241.herokuapp.com")  # этот url нужно заменить на url вашего Хероку приложения
             return "?", 200
 
         server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
