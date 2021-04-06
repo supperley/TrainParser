d0c3c662fcaa9eec2a204e94e6cfef735fa82150
Ladizzz <v.bobrovko@yandex.by>
2021-04-06 06:58:08 +0300

    Update 06.04

--- parser.py
+++ parser.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot('1778090744:AAEaEx2yVHAakqGrV-Sn8q-STE_bIJzSbPM')
 train_number = 0
 iteration = 0
 sleep_time = 60
-url = 'https://pass.rw.by/ru/route/?from=%D0%9E%D1%80%D1%88%D0%B0&from_exp=2100170&from_esr=166403&to=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&to_exp=2100000&to_esr=140210&front_date=%D1%81%D0%B5%D0%B3%D0%BE%D0%B4%D0%BD%D1%8F&date=today'
+url = 'https://pass.rw.by/ru/route/?from=%D0%9E%D1%80%D1%88%D0%B0&from_exp=2100170&from_esr=166403&to=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&to_exp=2100000&to_esr=140210&front_date=11+%D0%B0%D0%BF%D1%80.+2021&date=2021-04-11'
 
 
 @bot.message_handler(commands=['start'])
