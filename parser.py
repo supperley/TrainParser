c674ba58944295a9c3241da337b44d274c7b6ca4
Ladizzz <v.bobrovko@yandex.by>
2021-06-10 00:02:08 +0300

    Update parser.py
    10.06

--- parser.py
+++ parser.py
@@ -14,7 +14,8 @@ bot = telebot.TeleBot(token)
 train_number = 0
 iteration = 0
 sleep_time = 57
-url = 'https://pass.rw.by/ru/route/?from=%D0%9E%D1%80%D1%88%D0%B0&from_exp=2100170&from_esr=166403&to=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA-%D0%9F%D0%B0%D1%81%D1%81%D0%B0%D0%B6%D0%B8%D1%80%D1%81%D0%BA%D0%B8%D0%B9&to_exp=2100001&to_esr=&front_date=31+%D0%BC%D0%B0%D1%8F.+2021&date=2021-05-31'
+# url = 'https://pass.rw.by/ru/route/?from=%D0%9E%D1%80%D1%88%D0%B0&from_exp=2100170&from_esr=166403&to=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA-%D0%9F%D0%B0%D1%81%D1%81%D0%B0%D0%B6%D0%B8%D1%80%D1%81%D0%BA%D0%B8%D0%B9&to_exp=2100001&to_esr=&front_date=31+%D0%BC%D0%B0%D1%8F.+2021&date=2021-05-31'
+url = 'https://pass.rw.by/ru/route/?from=%D0%9E%D1%80%D1%88%D0%B0-%D0%A6%D0%B5%D0%BD%D1%82%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F&from_exp=&from_esr=&to=%D0%9F%D0%BE%D0%BB%D0%BE%D1%86%D0%BA&to_exp=&to_esr=&front_date=13+%D0%B8%D1%8E%D0%BD%D1%8F.+2021&date=2021-06-13'
 debug = False
 server = flask.Flask(__name__)
 
